b76ded9c47b3e22530680f5662d2ecc0041aace1
David_Izosimov <David.Izosimov@gmail.com>
2019-09-27 10:38:27 +0200

    Adds new html for new article, adds article controller

--- features/step_definitions/create_article_steps.rb
+++ features/step_definitions/create_article_steps.rb
@@ -1,23 +1,23 @@
-# Given(/^I am on the landing page$/) do
+# # Given(/^I am on the landing page$/) do
+# #     visit root_path
+# # end
+
+# Given("I visit the {string} page") do |landing|
 #     visit root_path
 # end
-
-Given("I visit the {string} page") do |landing|
-    visit root_path
-end
   
-  When("I click {string} link") do |content|
-    expect(page).not_to have_content content #Adds article to database and displays
-  end
-  
-  When("I fill in {string} with {string}") do |title, titlestring|
-    expect(title).to have_content titlestring # Write code here that turns the phrase above into concrete actions
-  end
-  
-  When("I click {string} button") do |title|
-    expect(page).not_to have_content title #Will not allow creating article due to missing title
-  end
-  
-#   When("I click {string} button") do |content|
-#     expect(page).not_to have_content content #Will not allow creating article due to missing content
+#   When("I click {string}") do |element|
+#     click_link element #Adds article to database and displays
 #   end
+  
+#   When("I fill in {string} with {string}") do |title, titlestring|
+#     expect(title).to have_content titlestring # Write code here that turns the phrase above into concrete actions
+#   end
+  
+#   When("I click {string} button") do |title|
+#     expect(page).not_to have_content title #Will not allow creating article due to missing title
+#   end
+  
+# #   When("I click {string} button") do |content|
+# #     expect(page).not_to have_content content #Will not allow creating article due to missing content
+# #   end
