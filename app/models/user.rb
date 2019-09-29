dcbf7fe90f73dc4e8c3e6908305e6fa9e347e75c
David_Izosimov <David.Izosimov@gmail.com>
2019-09-29 14:35:11 +0200

    Adds a functional navbar with option of login and signup, login and signup function not created

---
+++ app/models/user.rb
@@ -0,0 +1,20 @@
+class User < ApplicationRecord
+    # Include default devise modules. Others available are:
+    # :confirmable, :lockable, :timeoutable and :omniauthable
+    devise :database_authenticatable, :registerable,
+           :recoverable, :rememberable, :trackable, :validatable
+  
+    validates :name, length: { maximum: 20 }
+    validates :name, presence: true
+    validates_uniqueness_of :name
+  
+    acts_as_messageable
+  
+    def mailboxer_name
+      name
+    end
+  
+    def mailboxer_email(object)
+      email
+    end
+  end
